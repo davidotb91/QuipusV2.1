df9fa4daff9cd0bb646e70d5fda3af6dac7ffdaf
David Tayupanta <davidotb91@hotmail.com>
2016-01-19 16:46:17 -0500

    persistencia
    persistencia detalle rugbro

---
+++ src/com/ec/cliente/clienteRS.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ec.cliente;
+
+/**
+ * Jersey REST client generated for REST resource:DetalleRubroFacadeREST
+ * [com.ec.restfull.persistence.detallerubro]<br>
+ * USAGE:
+ * <pre>
+ *        clienteRS client = new clienteRS();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author david
+ */
+public class clienteRS {
+    
+}
